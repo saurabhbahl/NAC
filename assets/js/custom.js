be20c7dd28f990439d56c0910d2f9573228481cf
nehajolly1~ <behl.saurabh@gmail.com>
2024-10-19 17:59:15 +0530

    Fields updated

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        // $(this).toggleClass('in');
+       
+    });
+
+});
